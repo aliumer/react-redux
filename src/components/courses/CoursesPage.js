b70e9ed5e14d50614875cb5a30be34dfc4e92570
aliumer <ilaumer@gmail.com>
2024-11-16 13:08:21 +0000

    Added course delete optimistic.

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,87 +1,102 @@
 import React from "react";
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import Spinner from "../common/Spinner";
+import toast from "react-toastify";
 
 class CoursesPage extends React.Component {
-    state = {
-        redirectToAddCoursePage: false
-    };
+  state = {
+    redirectToAddCoursePage: false
+  };
 
-    componentDidMount() {
-        const { courses, authors, actions } = this.props;
+  componentDidMount() {
+    const { courses, authors, actions } = this.props;
 
-        if (courses.length === 0) {
-            actions.loadCourses.loadCourses().catch(error => {
-                alert("Loading Course Error: " + error);
-            })
-        }
-
-        if (authors.length === 0) {
-            actions.loadAuthors.loadAuthors().catch(error => {
-                alert("Loading Authors Error: " + error);
-            })
-        }
-
+    if (courses.length === 0) {
+      actions.loadCourses.loadCourses().catch(error => {
+        alert("Loading Course Error: " + error);
+      });
     }
 
-
-    render() {
-        return (
-            <>
-                {this.state.redirectToAddCoursePage && <Navigate to="/course" />}
-                <h2>Courses</h2>
-                {
-                    this.props.loading
-                        ? <Spinner />
-                        :
-                        <>
-                            <button style={{ marginBottom: 20 }} className="btn btn-primary add-course" onClick={() => this.setState({ redirectToAddCoursePage: true })}>Add Course</button>
-                            <CourseList courses={this.props.courses} />
-                        </>
-                }
-            </>
-        )
+    if (authors.length === 0) {
+      actions.loadAuthors.loadAuthors().catch(error => {
+        alert("Loading Authors Error: " + error);
+      });
     }
+  }
+
+  handleDeleteCourse = async course => {
+    toast.success("Course deleted successfully.");
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
+      toast.error("Delet failed with errors: " + error.message, {
+        autoClose: false
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.redirectToAddCoursePage && <Navigate to="/course" />}
+        <h2>Courses</h2>
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList courses={this.props.courses} />
+          </>
+        )}
+      </>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-    authors: PropTypes.array,
-    courses: PropTypes.array,
-    actions: PropTypes.object,
-    loading: PropTypes.bool.isRequired
-}
-
+  authors: PropTypes.array,
+  courses: PropTypes.array,
+  actions: PropTypes.object,
+  loading: PropTypes.bool.isRequired
+};
 
 function mapStateToProps(state) {
-    return {
-        authors: state.authors,
-        courses: state.authors.length === 0
-            ? []
-            : state.courses.map(course => {
-                return {
-                    ...course,
-                    authorName: state.authors.find(a => a.id === course.authorId).name
-                }
-            }),
-        loading: state.apiCallsInProgress > 0
-    }
+  return {
+    authors: state.authors,
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    loading: state.apiCallsInProgress > 0
+  };
 }
 
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        actions: {
-            loadCourses: bindActionCreators(courseActions, dispatch),
-            loadAuthors: bindActionCreators(authorActions, dispatch),
-        }
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: {
+      loadCourses: bindActionCreators(courseActions, dispatch),
+      loadAuthors: bindActionCreators(authorActions, dispatch),
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch)
     }
-}
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
 // dispatch property automatically injected by using mapStateToProps
--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -9,78 +9,81 @@ import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
 
-
 export default function ManageCoursePage() {
-    const courses = useSelector(state => state.courses);
-    const authors = useSelector(state => state.authors);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [course, setCourse] = useState(newCourse);
-    const { slug } = useParams();
-    const [saving, setSaving] = useState(false);
-    const [errors, setError] = useState();
+  const courses = useSelector(state => state.courses);
+  const authors = useSelector(state => state.authors);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [course, setCourse] = useState(newCourse);
+  const { slug } = useParams();
+  const [saving, setSaving] = useState(false);
+  const [errors, setError] = useState();
 
-    useEffect(() => {
-        if (courses.length === 0) {
-            dispatch(loadCourses()).catch(error => alert("Loading courses failed: " + error));
-        } else {
-            if (slug) {
-                setCourse(courses.find(c => c.slug === slug) || newCourse);
-            }
-        }
-    }, [courses, slug]);
-
-    useEffect(() => {
-        if (authors.length === 0) {
-            dispatch(loadAuthors()).catch(error => {
-                alert("Loading authors failed: " + error);
-            })
-        }
-    }, [authors])
-
-
-    function handleChange(event) {
-        const { name, value } = event.target
-        setCourse(prevCourse => ({
-            ...prevCourse,
-            [name]: name === "authorId" ? parseInt(value, 10) : value
-        }))
+  useEffect(() => {
+    if (courses.length === 0) {
+      dispatch(loadCourses()).catch(error =>
+        alert("Loading courses failed: " + error)
+      );
+    } else {
+      if (slug) {
+        setCourse(courses.find(c => c.slug === slug) || newCourse);
+      }
     }
+  }, [courses, slug]);
 
-    function formIsValid() {
-        const { title, authorId, category } = course;
-        const errors = {};
-        if (!title) errors.title = "Title is required.";
-        if (!authorId) errors.authorId = "Author Id is required.";
-        if (!category) errors.category = "Category is required.";
-
-        setError(errors);
-
-        return Object.keys(errors).length === 0;
+  useEffect(() => {
+    if (authors.length === 0) {
+      dispatch(loadAuthors()).catch(error => {
+        alert("Loading authors failed: " + error);
+      });
     }
+  }, [authors]);
 
-    function handleSave(event) {
-        event.preventDefault();
-        if (!formIsValid) return;
-        setSaving(true);
-        dispatch(saveCourse(course)).then(() => {
-            toast.success("Course saved!");
-            navigate("/courses");
-        }).catch(error => {
-            setSaving(false);
-            setError({ onSave: error.message });
-        });
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  }
 
-    return authors.length === 0 || courses.length === 0 ? (<Spinner />) :
-        (
-            <CourseForm
-                authors={authors}
-                course={course}
-                errors={errors}
-                saving={saving}
-                onChange={handleChange}
-                onSave={handleSave} />
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author Id is required.";
+    if (!category) errors.category = "Category is required.";
 
-        )
+    setError(errors);
+
+    return Object.keys(errors).length === 0;
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    if (!formIsValid) return;
+    setSaving(true);
+    dispatch(saveCourse(course))
+      .then(() => {
+        toast.success("Course saved!");
+        navigate("/courses");
+      })
+      .catch(error => {
+        setSaving(false);
+        setError({ onSave: error.message });
+      });
+  }
+
+  return authors.length === 0 || courses.length === 0 ? (
+    <Spinner />
+  ) : (
+    <CourseForm
+      authors={authors}
+      course={course}
+      errors={errors}
+      saving={saving}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
 }
